5231d4f5ce58a577afdd8939543a40d6e1025804
Jason Donmoyer <jasondonmoyer@Mac.lan>
2026-04-24 04:09:25 -0400

    Fixed missing alt descriptions: - Hero image - Three images in Services component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,12 @@ export default function Hero() {
     return (
         <div>
             <h1>Sparkle & Shine Cleaning Co.</h1>
-            <img src="/office-cleaning.webp" loading="eager" style={{ width: '100%', objectFit: 'cover' }} />
+            <img 
+                src="/office-cleaning.webp" 
+                loading="eager" 
+                style={{ width: '100%', objectFit: 'cover' }} 
+                alt="gloved hands cleaning a desk in an office"
+            />
         </div>
     )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,21 +4,33 @@ export default function Services() {
             <h2>Services Offered</h2>
             <div>
                 <h2>House Cleaning</h2>
-                <img src="/house-cleaning.webp" style={{ width: '100%' }} />
+                <img 
+                    src="/house-cleaning.webp" 
+                    style={{ width: '100%' }}
+                    alt="gloved hands spraying cleaner on surface" 
+                />
                 <div>
                     <p>Price: <span>$50/cleaning</span></p>
                 </div>
             </div>
             <div>
                 <h2>Deep Clean</h2>
-                <img src="/vacuum.webp" style={{ width: '100%' }} />
+                <img 
+                    src="/vacuum.webp" 
+                    style={{ width: '100%' }} 
+                    alt="vacuum cleaner being used on a dirty carpet"
+                />
                 <div>
                     <p>Price: <span>$100/cleaning</span></p>
                 </div>
             </div>
             <div>
                 <h2>Moving In / Moving Out Cleaning</h2>
-                <img src="/clean-kitchen.webp" style={{ width: '100%' }} />
+                <img 
+                    src="/clean-kitchen.webp" 
+                    style={{ width: '100%' }} 
+                    alt="spotless clean kitchen sink and faucet"
+                />
                 <div>
                     <p>Price: <span>$125/cleaning</span></p>
                 </div>
